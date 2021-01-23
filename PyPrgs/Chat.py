c2fa91e54704788e423c8e9cc1b518ad6e502563
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2021-01-24 00:44:29 +0100

    Alle widgets sind jetzt Klassenattribute

--- PyPrgs/Chat.py
+++ PyPrgs/Chat.py
@@ -7,26 +7,27 @@ class MenubuttonFrame(ttk.Frame):
     def __init__(self, master = None): 
         ttk.Frame.__init__(self, master)
 
-        btn_refresh  = ttk.Button(self, text="Refresh" )
-        btn_refresh.grid(row=0, column=0)
+        self.btn_refresh  = ttk.Button(self, text="Refresh" )
+        self.btn_refresh.grid(row=0, column=0)
 
-        btn_settings = ttk.Button(self, text="Settings") 
-        btn_settings.grid(row=0, column=1)
+        self.btn_settings = ttk.Button(self, text="Settings") 
+        self.btn_settings.grid(row=0, column=1)
         
-        btn_finish = ttk.Button(self, text="Quit" ) 
-        btn_finish.grid(row=0, column=2)
+        self.btn_finish = ttk.Button(self, text="Quit" ) 
+        self.btn_finish.grid(row=0, column=2)
 
 class InputBar(ttk.Frame): 
 
     def __init__(self, master = None): 
         ttk.Frame.__init__(self, master)
 
-        inputField  = ttk.Entry(self)
-        inputField.grid(row=0, column=0, sticky=tk.E+tk.W )
+        self.inputField  = ttk.Entry(self)
+        self.inputField.grid(row=0, column=0, sticky=tk.E+tk.W )
 
-        btn_submit = ttk.Button(self, text="Submit") 
-        btn_submit.grid(row=0, column=1, sticky=tk.E+tk.W)
+        self.btn_submit = ttk.Button(self, text="Submit") 
+        self.btn_submit.grid(row=0, column=1, sticky=tk.E+tk.W)
         
+        # textbar soll all den verfügbaren platz der row bekommen
         self.columnconfigure(0, weight=1)
         
 
