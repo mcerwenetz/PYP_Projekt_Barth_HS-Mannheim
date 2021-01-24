b350ae3012252dfd548d02431d959d655b9e2134
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2021-01-24 11:23:49 +0100

    Dokumentation hinzugefügt

--- PyPrgs/Chat.py
+++ PyPrgs/Chat.py
@@ -5,13 +5,27 @@ import urllib.parse
 from ttkthemes import ThemedTk
 CHAT_URL = "https://pma.inftech.hs-mannheim.de/wsgi/chat"
 
+class MenubuttonFrame(ttk.Frame):
+    """Dieses Frame ist oben im Chatfenster.
 
-class MenubuttonFrame(ttk.Frame): 
-
+    Attributes
+    -------------
+    btn_refresh : ttk.Button
+        Wenn man diesen Button drückt wird der Chat manuell
+        aktualisiert.
+    
+    btn_settings : ttk.Button
+        Wenn man diesen Button drückt werden die Einstellungen
+        als Toplevel gestartet.
+    
+    btn_finish : ttk.Button
+        Wenn man diesen Button drückt wird das Programm beendet.
+    """
     def __init__(self, master = None): 
         ttk.Frame.__init__(self, master)
 
-        self.btn_refresh  = ttk.Button(self, text="Refresh", command=Chat.quit_cmd)
+        self.btn_refresh  = ttk.Button(self, text="Refresh",
+         command=Chat.quit_cmd)
         self.btn_refresh.grid(row=0, column=0)
 
         self.btn_settings = ttk.Button(self, text="Settings") 
@@ -20,8 +34,19 @@ class MenubuttonFrame(ttk.Frame):
         self.btn_finish = ttk.Button(self, text="Quit" ) 
         self.btn_finish.grid(row=0, column=2)
 
-class InputBar(ttk.Frame): 
+class InputBar(ttk.Frame):
+    """
+    Dieses Frame ist die Eingabezeile unten im Chat.
 
+    Attributes
+    ------------
+    inputField : ttk.Entry
+        Das Haupteingabefeld für den Chat
+
+    btn_submit : ttk.Entry
+        Wenn man diesen Button drückt wird der Inhalt des inputFields
+        geposted
+    """
     def __init__(self, master = None): 
         ttk.Frame.__init__(self, master)
 
@@ -35,9 +60,53 @@ class InputBar(ttk.Frame):
         self.columnconfigure(0, weight=1)
         
 
-class Chat(ThemedTk): 
+class Chat(ThemedTk):
+    """
+    Hauptklasse des Chats. Das Mainframe in dem alle Unterframes gebunden
+    werden.
     
-    def __init__(self, theme = 'default'): 
+    Attributes
+    -----------
+    menubuttons : MenubuttonFrame
+        Die Menubuttons oben im Chat (Refresh, Close, Settings)
+
+    lbl_chatview : tk.Label
+        Der Chatverlauf
+
+    inputBar : InputBar
+        Die Eingabezeile unten.
+
+    Methods
+    -----------
+    create_widgets(self)
+        Diese Methode erstellt alle Frames und Widgets
+        im Chatfenster.
+
+    quit_cmd(self)
+        Schließt das Chatfenster
+
+    refresh(self)
+        Aktualisiert den Chatverlauf
+
+    chat_get(self)
+        Holt den Chatverlauf vom Server
+
+    chat_post(msg, usr = "Anon")
+        Posted einen String, die Message, auf den Server mit
+        unter dem username usr
+        default ist Anon.
+    """
+
+    def __init__(self, theme = 'default'):
+        """
+        Initialisierung des Mainframes.
+        Stylekonfiguration
+        Initialisierung und bindung der Subframes.
+        Parameters
+        ----------
+        theme : str, optional, default='default'
+            Das ttk Theme.
+        """
         ThemedTk.__init__(self)
         
         #title of window            
@@ -53,7 +122,10 @@ class Chat(ThemedTk):
 
         self.create_widgets()
 
-    def create_widgets(self): 
+    def create_widgets(self):
+        """
+        Initialisierung und Bindung der Subframes.
+        """
         #Buttonreihe oben   
         self.menubuttons = MenubuttonFrame(self)
         self.menubuttons.grid(column=0, row=0, sticky=tk.W)
@@ -72,12 +144,13 @@ class Chat(ThemedTk):
         #alle rows brauchen weight für resize in x richtung
         self.columnconfigure(0,weight=1)
     
-    def quit_cmd(self): 
+    def quit_cmd(self):
+        """Beenden des Mainframes."""
         self.destroy()
         
 
-
-    def refresh(self): 
+    def refresh(self):
+        """Aktualisierung des Chatverlaufs"""
         msgs = chat_get()
         msgs = list(msgs.splitlines())
         msgs.reverse()
@@ -87,13 +160,15 @@ class Chat(ThemedTk):
             print(m)
 
 
-    def chat_get():
+    def chat_get(self):
+        """Chatverlauf von Server holen."""
         resp = urllib.request.urlopen(CHAT_URL)
         if not resp.status==200: 
             return None
         return resp.read().decode("UTF-8")
 
     def chat_post(msg, usr = "Anon"):
+        """Neue Nachricht auf Server posten"""
         msg = "["+usr+"] " + msg 
         data = msg.encode("UTF-8")
         
